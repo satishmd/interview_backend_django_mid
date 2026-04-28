c76e1fc51ed8a6b21ca1a42926d7d86638afc6fd
satish <mdsatishkumar145@gmail.com>
2026-04-28 10:37:54 +0530

    Profile model

--- interview/inventory/admin.py
+++ interview/inventory/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Inventory, InventoryType, InventoryTag, InventoryLanguage
 
-# Register your models here.
+admin.site.register(Inventory)
+admin.site.register(InventoryType)
+admin.site.register(InventoryTag)
+admin.site.register(InventoryLanguage)
---
+++ profiles/models.py
@@ -0,0 +1,53 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.db import models
+
+
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_admin", True)
+
+        return self.create_user(email, password, **extra_fields)
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, blank=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def get_full_name(self):
+        return f"{self.first_name} {self.last_name}".strip()
+
+    def get_username(self):
+        return self.email
+
+    @property
+    def is_authenticated(self):
+        return True
+
+    def __str__(self):
+        return self.email
